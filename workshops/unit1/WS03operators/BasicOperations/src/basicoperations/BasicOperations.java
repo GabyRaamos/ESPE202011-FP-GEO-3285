ca33c664dc7f5b3af82a8c8da5e0034ccd81b0e8
GabyRaamos <lovemegaby2000@hotmail.com>
2021-01-06 16:39:58 -0500

    workshops 03operators

--- workshops/unit1/WS03operators/BasicOperations/src/basicoperations/BasicOperations.java
+++ workshops/unit1/WS03operators/BasicOperations/src/basicoperations/BasicOperations.java
@@ -26,6 +26,22 @@ public class BasicOperations {
         int subtrahend;
         int difference;
         
+        int multiplication;
+        int multiplicant;
+        int multiplier; 
+        
+        int division;
+        int dividend;
+        int divisor;
+        
+        int modulus; 
+        
+        dividend = 20;
+        divisor = 5;
+        
+        multiplicant =9;
+        multiplier = 6;
+        
         minuend = -23;
         subtrahend = -6;
         
@@ -35,19 +51,29 @@ public class BasicOperations {
         //operation
         sum = addend1 + addend2;
         
-        System.out.println( "The addition of " + addend1 + " + " + addend2 + " is equal to >> " + sum);
+        System.out.println( " The addition of " + addend1 + " + " + addend2 + " is equal to >> " + sum);
        
     ++sum;
-    System.out.println (" sum is equal to >> " + sum);
+    System.out.println (" Sum is equal to >> " + sum);
     
    sum++;
-   System.out.println(" sum is equal to >> " +sum);
+   System.out.println(" Sum is equal to >> " +sum);
    
    System.out.println(" addind one to sum " + (sum++));
    
    System.out.println(" adding one to sum " + (++sum));
            
-   System.out.println(" the subtraction of " + minuend + " - " + subtrahend + " is equal to " + (minuend - subtrahend));
+   System.out.println(" The subtraction of " + minuend + " - " + subtrahend + " is equal to " + (minuend - subtrahend));
+   
+    multiplication = multiplicant * multiplier;
+        
+        System.out.println( " the product of the multiplication " + multiplicant + " * " + multiplier + " is equal to >> " + multiplication);
+       
+           
+        System.out.println(" The quotient of the division " + dividend + "/" + divisor + " is equal to >> " + ( dividend / divisor ));
+   
+        System.out.println(" The residue of the division " + dividend + "/" + divisor + " is equal to >>" + (dividend %divisor));
+    
    
     
 }
